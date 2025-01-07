aae83c1d5025697e38e6fcf9afd88f2869d18360
Nigel Feng <10083@holbertonstudents.com>
2025-01-07 12:06:38 +0000

    9. Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,28 +8,28 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-  size_t left_height, right_height;
-  
-  if (tree == NULL)
-    {
-      return (0);
-    }
-  else
-    {
-      if (tree->left == NULL && tree->right == NULL)
-	{
-	  return (0);
-	}
-      left_height = binary_tree_height(tree->left);
-      right_height = binary_tree_height(tree->right);
- 
-      if (left_height >= right_height)
-	{
-	  return (left_height + 1);
-	}
-      else
-	{
-	  return (right_height + 1);
-	}
-    }
+size_t left_height, right_height;
+
+if (tree == NULL)
+{
+return (0);
+}
+else
+{
+if (tree->left == NULL && tree->right == NULL)
+{
+return (0);
+}
+left_height = binary_tree_height(tree->left);
+right_height = binary_tree_height(tree->right);
+
+if (left_height >= right_height)
+{
+return (left_height + 1);
+}
+else
+{
+return (right_height + 1);
+}
+}
 }
