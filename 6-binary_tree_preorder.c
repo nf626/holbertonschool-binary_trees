2276bee61c749a60aefef8309d03e13f7ef0fc0c
Nigel Feng <10083@holbertonstudents.com>
2025-01-07 07:55:52 +0000

    6. Pre-order traversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -8,17 +8,17 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-  if (tree != NULL && func != NULL)
-    {
-      func(tree->n);
+if (tree != NULL && func != NULL)
+{
+func(tree->n);
 
-      if (tree->left != NULL)
-	{
-	  binary_tree_preorder(tree->left, func);
-	}
-      if (tree->right != NULL)
-	{
-	  binary_tree_preorder(tree->right, func);
-	}
-    }
+if (tree->left != NULL)
+{
+binary_tree_preorder(tree->left, func);
+}
+if (tree->right != NULL)
+{
+binary_tree_preorder(tree->right, func);
+}
+}
 }
