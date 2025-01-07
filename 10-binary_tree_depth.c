1a2ea54ca1a12971e843ae770991ed3b8b8e7ccb
Nigel Feng <10083@holbertonstudents.com>
2025-01-07 13:37:10 +0000

    10. Depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,17 +8,17 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-  size_t edge_depth = 0;
-  
-  if (tree == NULL)
-    {
-      return (0);
-    }
+size_t edge_depth = 0;
 
-  while (tree->parent != NULL)
-    {
-      tree = tree->parent;
-      edge_depth = edge_depth + 1;
-    }
-  return (edge_depth);
+if (tree == NULL)
+{
+return (0);
+}
+
+while (tree->parent != NULL)
+{
+tree = tree->parent;
+edge_depth = edge_depth + 1;
+}
+return (edge_depth);
 }
