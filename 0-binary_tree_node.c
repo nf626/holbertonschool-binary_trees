e3ee879ff77d5259135fe0530ee1702b849adfe3
Nigel Feng <10083@holbertonstudents.com>
2025-01-07 03:47:33 +0000

    0. New node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,17 +9,11 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-  binary_tree_t *key = malloc(sizeof(binary_tree_t));
-  if (key == NULL)
-    {
-      free(key);
-      return (NULL);
-    }
+  binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-  key = parent;
-  parent->n = value;
-  parent->left = NULL;
-  parent->right = NULL;
+  new_node->n = value;
+  new_node->parent = parent;
+  new_node->left = new_node->right = NULL;
   
-  return (key);
+  return (new_node);
 }
