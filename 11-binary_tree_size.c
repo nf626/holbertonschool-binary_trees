eb06946bb30d7f5928a735c8f259d910cb727188
Nigel Feng <10083@holbertonstudents.com>
2025-01-07 13:50:01 +0000

    11. Size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,12 +8,12 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-  if (tree == NULL)
-    {
-      return (0);
-    }
-  else
-    {
-      return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
-    }
+if (tree == NULL)
+{
+return (0);
+}
+else
+{
+return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
 }
