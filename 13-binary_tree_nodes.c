eeed34a96f7a3163bdb2f58e8efc63ee3397d52c
Nigel Feng <13. Nodes>
2025-01-08 01:05:35 +0000

    13. Nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -46,12 +46,12 @@ return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-  if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-    {
-      return (0);
-    }
-  else
-    {
-      return (binary_tree_size(tree) - binary_tree_leaves(tree));
-    }
+if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+{
+return (0);
+}
+else
+{
+return (binary_tree_size(tree) - binary_tree_leaves(tree));
+}
 }
