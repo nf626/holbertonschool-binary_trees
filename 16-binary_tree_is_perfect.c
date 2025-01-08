5b24d3fc051ba0129cfd974e8a0aa15b183f4bc3
Nigel Feng <13. Nodes>
2025-01-08 04:15:34 +0000

    16. Is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,18 +8,18 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-  if (tree != NULL)
+  if (tree == NULL)
     {
-      if (tree->left == NULL && tree->right == NULL)
-	{
-	  return (1);
-	}
-      
-      if (tree->left == NULL || tree->right == NULL)
-	{
-	  return (0);
-	}
-      return ((binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right)));
+      return (0);
     }
-  return (0);
+
+  if (tree->left == NULL && tree->right == NULL)
+    {
+      return (1);
+    }
+  if (tree->left == NULL || tree->right == NULL)
+    {
+      return (0);
+    }
+  return ((binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right)));
 }
