f884bfc3cc6694791a88b21464f3cc4953e72d20
Nigel Feng <13. Nodes>
2025-01-08 06:25:56 +0000

    17. Sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,16 +8,16 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-  if (node == NULL || node->parent == NULL)
-    {
-      return (NULL);
-    }
-  if (node == node->parent->left)
-    {
-      return (node->parent->right);
-    }
-  else
-    {
-      return (node->parent->left);
-    }
+if (node == NULL || node->parent == NULL)
+{
+return (NULL);
+}
+if (node == node->parent->left)
+{
+return (node->parent->right);
+}
+else
+{
+return (node->parent->left);
+}
 }
